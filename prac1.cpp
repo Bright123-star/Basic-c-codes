442c70df6c232cbb40378461a8e66f15ceb6b24a
Nana Sarfo <sarfonanayaw11@gmail.com>
2025-12-29 15:17:09 +0000

    Add files via upload

---
+++ prac1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+	int p=2.5+3.2;
+	cout<<p;
+}
+
+//int results(int num1,int num2,int num3){
+//	num2=pow(num2,3);
+//	num1=pow(num1,3);
+//	num3=pow(num3,3);
+//	int result= num1+num2+num3;
+//    cout<<result;
+//    return 0;
+//}
+///* run this program using the console pauser or add your own getch, system("pause") or input loop */
+//
+//int main(int argc, char** argv) {
+//	int num1,num2,num3,result;
+//	
+//	cout<<"WElcome to armstrong numbers"<<endl;
+//	cout<<"lets calculate the armstrong of 3 numbers"<<endl;
+//	cout<<"Enter First number "<<endl;
+//	cin>>num1;
+//	cout<<"Enter First number "<<endl;
+//	cin>>num2;
+//	cout<<"Enter First number "<<endl;
+//	cin>>num3;
+//	results(num1,num2,num3);
+//	return 0;
+//
+//
+
+//int main(){
+//	
+//int num,originalnum,reverednum=0,remainder;
+//cout<<"Enter a number"<<endl;
+//cin>>num;
+//originalnum=num;
+//while(num!=0){
+//	remainder=num%10;
+//	reverednum=reverednum*10+remainder;
+//	num1=10;
+//}
+//if 
+//}
